1216dfc6657fcd30f3e6e4fd3c5cc9a92c3a85e1
Zhiwen Xie <TiwazBiu@gmail.com>
2019-12-10 17:25:21 +0800

    completed lab09: Scheme

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,68 +1,85 @@
-;; Scheme ;;
-
+; ; Scheme ;;
 (define (over-or-under x y)
-  'YOUR-CODE-HERE
-)
+  (cond 
+    ((= x y) 0)
+    ((< x y) -1)
+    ((> x y) 1)))
 
-;;; Tests
+; ;; Tests
 (over-or-under 1 2)
+
 ; expect -1
 (over-or-under 2 1)
+
 ; expect 1
 (over-or-under 1 1)
+
 ; expect 0
-
 (define (filter-lst f lst)
-  'YOUR-CODE-HERE
-)
+  (cond 
+    ((null? lst)
+     lst)
+    ((f (car lst))
+     (cons (car lst) (filter-lst f (cdr lst))))
+    (else
+     (filter-lst f (cdr lst)))))
 
-;;; Tests
-(define (even? x)
-  (= (modulo x 2) 0))
+; ;; Tests
+(define (even? x) (= (modulo x 2) 0))
+
 (filter-lst even? '(0 1 1 2 3 5 8))
+
 ; expect (0 2 8)
+(define (make-adder num) (lambda (x) (+ x num)))
 
-(define (make-adder num)
-  'YOUR-CODE-HERE
-)
-
-;;; Tests
+; ;; Tests
 (define adder (make-adder 5))
+
 (adder 8)
+
 ; expect 13
-
-;; Extra questions
-
+; ; Extra questions
 (define lst
-  'YOUR-CODE-HERE
-)
+        (cons (cons 1 nil)
+              (cons 2 (cons (cons 3 (cons 4 nil)) (cons 5 nil)))))
 
-(define (composed f g)
-  'YOUR-CODE-HERE
-)
+(define (composed f g) (lambda (x) (f (g x))))
 
 (define (remove item lst)
-  'YOUR-CODE-HERE
-)
+  (filter-lst (lambda (x) (not (= x item))) lst))
 
-
-;;; Tests
+; ;; Tests
 (remove 3 nil)
+
 ; expect ()
 (remove 3 '(1 3 5))
+
 ; expect (1 5)
 (remove 5 '(5 3 5 5 1 4 5 4))
-; expect (3 1 4 4)
 
+; expect (3 1 4 4)
 (define (no-repeats s)
-  'YOUR-CODE-HERE
-)
+  (if (or (null? s) (null? (cdr s)))
+      s
+      (cons (car s)
+            (filter-lst (lambda (x) (not (= x (car s))))
+                        (no-repeats (cdr s))))))
 
 (define (substitute s old new)
-  'YOUR-CODE-HERE
-)
-
+  (cond 
+    ((null? s)
+     s)
+    ((pair? (car s))
+     (cons (substitute (car s) old new)
+           (substitute (cdr s) old new)))
+    ((eq? (car s) old)
+     (cons new (substitute (cdr s) old new)))
+    (else
+     (cons (car s) (substitute (cdr s) old new)))))
 
 (define (sub-all s olds news)
-  'YOUR-CODE-HERE
-)
+  (if (null? olds)
+      s
+      (sub-all (substitute s (car olds) (car news))
+               (cdr olds)
+               (cdr news))))
