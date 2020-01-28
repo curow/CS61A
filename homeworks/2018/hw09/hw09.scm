10f798d093aacdfd6ee8882d515a95323f7f7ec1
Zhiwen Xie <TiwazBiu@gmail.com>
2020-01-28 08:03:51 +0800

    finish homework09 with only one question
        define a macro to mimic the list comprehension
        feature of python

--- homeworks/2018/hw09/hw09.scm
+++ homeworks/2018/hw09/hw09.scm
@@ -1,3 +1,9 @@
 (define-macro (list-of map-expr for var in lst if filter-expr)
-  'YOUR-CODE-HERE
+  `(map (lambda (,var) ,map-expr) (filter (lambda (,var) ,filter-expr) ,lst))
 )
+
+; (map (lambda (var) map-expr) (filter (lambda (var) filter-expr) lst))
+
+; (list-of (* x x) for x in '(3 4 5) if (odd? x))
+
+; (map (lambda (x) (* x x)) (filter (lambda (x) (odd? x)) '(3 4 5)))
