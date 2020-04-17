6e2eaafc010bddf04cbbdabfc0d4e080acc3ecf6
Zhiwen Xie <TiwazBiu@gmail.com>
2020-04-17 11:05:27 +0800

    modified prime.py to get mutual prime from iterable rather a range

--- experiments/prime.py
+++ experiments/prime.py
@@ -4,10 +4,16 @@ def mutual_primes(int_iterator):
     yield x
     yield from mutual_primes(filter(lambda i: math.gcd(x, i) == 1, int_iterator))
 
-def print_mutual_primes(lo, hi):
-    int_iterator = iter(range(lo, hi))
+def print_mutual_primes(iterable):
+    int_iterator = iter(iterable)
     try:
         for x in mutual_primes(int_iterator):
             print(x)
     except:
         pass
+
+def test1():
+    print_mutual_primes(range(2, 10))
+
+def test2():
+    print_mutual_primes([3, 7, 10, 11])
